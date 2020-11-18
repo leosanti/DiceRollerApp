1d6fe666879c1ab16a1f2073664511877e25a3be
Leandro Santiago <leandro.s4nt1@gmail.com>
2020-11-18 13:51:29 -0400

    Primeiro projeto android  para o github

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class Dice {
+    fun roll(): Int {
+        return (1..6).random()
+    }
+}
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button3)
+
+        rollButton.setOnClickListener {
+            rollDice()
+            //val resultTextWiew: TextView = findViewById(R.id.textView2)
+            //resultTextWiew.text = "6"
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice()
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+    }
+}
